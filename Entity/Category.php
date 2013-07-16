2db87caf5f2bbba6ac9b1c4b5722716f22008e14
dallas62 <dallas62@NigthMare-DB>
2013-07-16 11:15:55 +0200

    Manager

---
+++ Entity/Category.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * This file is part of the Blackengine package.
+ *
+ * (c) Alexandre Balmes <albalmes@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Black\Bundle\LinkBundle\Document;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Black\Bundle\EngineBundle\Traits\ThingDocumentTrait;
+use Black\Bundle\LinkBundle\Model\Category as BaseCategory;
+
+/**
+ * Category Document
+ *
+ * @ODM\MappedSuperclass()
+ */
+class Category extends BaseCategory
+{
+    use ThingDocumentTrait;
+}
